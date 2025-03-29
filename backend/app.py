4cce7f318fc46710073127c4a9595fb09d5127b4
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2025-03-29 18:14:44 -0400

    Initialized basic data transfer between frontend ina backend

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import langchain as lc
+
+# Load environment variables from .env file
+load_dotenv()
+
+app=Flask(__name__)
+
+# Load api key
+API_KEY=os.getenv('API_KEY')
+
+# Allow React (frontend on port 3000) to access the backend
+CORS(app)
+
+@app.route('/')
+def main_page():
+    return jsonify({'message': API_KEY})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
