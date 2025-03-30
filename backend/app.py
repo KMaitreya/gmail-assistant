170d52cbbe4f8b07df3a42ea673075da89cc8b44
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2025-03-30 04:15:56 -0400

    Success in sending data to backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -14,10 +14,18 @@ API_KEY=os.getenv('API_KEY')
 
 # Allow React (frontend on port 3000) to access the backend
 CORS(app)
+data=None
 
-@app.route('/')
-def main_page():
-    return jsonify({'message': "We are ready to go"})
+@app.route('/response')
+def response():
+    return jsonify({'message': data})
+
+@app.route('/message', methods=['POST'])
+def message():
+    raw=request.get_json()
+    data=raw.get('message')
+    print(data, raw)
+    return jsonify({'message': raw})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+
 export function Input() {
 
+    const [message, setMessage] = useState("");
+
+    const handleChange=(e) => {
+        setMessage(e.target.value);
+    };
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+
+        if (message.trim()){
+            try{
+                await axios.post('http://127.0.0.1:5001/message',{message});
+                console.log("Message sent to backend:", message);
+            }
+            catch (error){
+                console.error("Error sending message to backend:", error);
+            }
+        }
+        setMessage("");
+    };
+
     return (
-        <form className="w-full mt-8">
+        <form onSubmit={handleSubmit} className="w-full mt-8">
             <label htmlFor="inputField" className="sr-only">Enter your message</label>
             <div className="relative">
                 <input 
                 type="text" 
                 id="inputField" 
+                value={message}
+                onChange={handleChange}
                 className="w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                 placeholder="Enter your message" 
                 required 
