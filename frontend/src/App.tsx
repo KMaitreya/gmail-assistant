d5317ea141b8708e55bcb54a202f64d3a3b185cb
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2025-03-31 03:09:22 -0400

    Updates to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import {Navigation} from './components/navbar.tsx';
-import {Backend} from './components/tempdata.tsx';
 import {Chat} from './components/chat.tsx';
 
 export default function App() {
   return(
     <div>
       <Navigation />
-      <Backend />
       <Chat />
     </div>
 
